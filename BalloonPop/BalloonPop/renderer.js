982bb0e8c7533fc2b31709f1d07970a143e01e43
SVladkov <s.v.vladkov@gmail.com>
2014-06-12 22:58:45 +0300

    Draw some objects
    We have an object moving by itself and an object moved by the arrows.
    It's just for test reasons.

--- BalloonPop/BalloonPop/renderer.js
+++ BalloonPop/BalloonPop/renderer.js
@@ -1,1 +1,86 @@
 ﻿// The renderer takes care of drawing the objects in the browser
+
+var oldWindowLoad = window.onload,
+    canvas = document.getElementById('field'),
+    ctx = canvas.getContext('2d');
+
+function drawPoint(point) {
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, point.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "green";
+    ctx.fill();
+}
+
+function changePosition(ball, traectory) {
+    ball.x = traectory.x + traectory.radius * Math.cos(traectory.angle);
+    ball.y = traectory.y + traectory.radius * Math.sin(traectory.angle);
+}
+
+function drawMovingBalloon(balloon, traectory) {
+    traectory.angle += 0.05;
+
+    changePosition(balloon, traectory);
+    drawPoint(balloon);
+
+    ctx.beginPath();
+    ctx.arc(traectory.x, traectory.y, traectory.radius, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function player(player1) {
+    drawPoint(player1);
+
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+
+        e = e || window.event;
+
+        if (e.keyCode == '37') {
+            player1.x -= 10;
+        }
+        else if (e.keyCode == '38') {
+            player1.y -= 10;
+        }
+        else if (e.keyCode == '39') {
+            player1.x += 10;
+        }
+        else if (e.keyCode == '40') {
+            player1.y += 10;
+        }
+    }
+}
+
+window.onload = function () {
+    //oldWindowLoad();
+    //player();
+
+    var traectory = {
+        x: canvas.width / 2,
+        y: canvas.height / 2,
+        radius: 150,
+        angle: 0
+    }
+    var balloon = {
+        radius: 5,
+        x: 0,
+        y: 0
+    };
+    var player1 = {
+        radius: 30,
+        x: 100,
+        y: 200,
+    };
+
+    function frame() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        drawMovingBalloon(balloon, traectory);
+        player(player1);
+
+        window.requestAnimationFrame(frame);
+    }
+
+    frame();
+}
+
